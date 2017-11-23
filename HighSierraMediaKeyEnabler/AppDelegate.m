8208cc40169f460823ac51a030d0dab659126757
Milan Toth <milgra@milgra.com>
2017-11-23 16:47:49 +0100

    version 1.2 - stripped down code, new icons, developer ID signing

--- HighSierraMediaKeyEnabler/AppDelegate.m
+++ HighSierraMediaKeyEnabler/AppDelegate.m
@@ -4,70 +4,114 @@
     @interface AppDelegate ()
     {
         NSStatusItem* statusItem;
+        CFMachPortRef eventPort;
+        CFRunLoopSourceRef eventPortSource;
     }
+
     @end
 
     @implementation AppDelegate
+
+    static CGEventRef tapEventCallback(CGEventTapProxy proxy, CGEventType type, CGEventRef event, void *refcon)
+    {
+        AppDelegate *self = (__bridge id)refcon;
+
+        if(type == kCGEventTapDisabledByTimeout)
+        {
+            CGEventTapEnable(self->eventPort, TRUE);
+            return event;
+        }
+        else if(type == kCGEventTapDisabledByUserInput)
+        {
+            return event;
+        }
+        
+        NSEvent *nsEvent = nil;
+        @try
+        {
+            nsEvent = [NSEvent eventWithCGEvent:event];
+        }
+        @catch (NSException * e) {
+            return event;
+        }
+
+        if (type != NX_SYSDEFINED || [nsEvent subtype] != 8)
+            return event;
+
+        int keyCode = (([nsEvent data1] & 0xFFFF0000) >> 16);
+        if (keyCode != NX_KEYTYPE_PLAY && keyCode != NX_KEYTYPE_FAST && keyCode != NX_KEYTYPE_REWIND && keyCode != NX_KEYTYPE_PREVIOUS && keyCode != NX_KEYTYPE_NEXT)
+            return event;
+        
+        int keyFlags = ([nsEvent data1] & 0x0000FFFF);
+        BOOL keyIsPressed = (((keyFlags & 0xFF00) >> 8)) == 0xA;
+
+        if (keyIsPressed)
+        {
+            iTunesApplication* iTunes = [SBApplication applicationWithBundleIdentifier:@"com.apple.iTunes"];
+            SpotifyApplication *spotify = [SBApplication applicationWithBundleIdentifier:@"com.spotify.client"];
+
+            switch (keyCode) {
+                case NX_KEYTYPE_PLAY:
+                {
+                    if ( [iTunes isRunning ] ) [iTunes playpause];
+                    if ( [spotify isRunning ] ) [spotify playpause];
+                    break;
+                }
+                case NX_KEYTYPE_FAST:
+                {
+                    if ( [iTunes isRunning ] ) [iTunes nextTrack];
+                    if ( [spotify isRunning ] ) [spotify nextTrack];
+                    break;
+                }
+                case NX_KEYTYPE_REWIND:
+                {
+                    if ( [iTunes isRunning ] ) [iTunes previousTrack];
+                    if ( [spotify isRunning ] ) [spotify previousTrack];
+                    break;
+                }
+            }
+        }
+        // stop propagation
+        return NULL;
+    }
 
     - ( void ) applicationDidFinishLaunching : ( NSNotification*) theNotification
     {
         NSMenu *menu = [ [ NSMenu alloc ] init ];
         [ menu addItemWithTitle : @"Running" action : nil keyEquivalent : @"" ];
         [ menu addItem : [ NSMenuItem separatorItem ] ]; // A thin grey line
-        [ menu addItemWithTitle : @"Quit" action : @selector(terminate:) keyEquivalent : @"" ];
+        [ menu addItemWithTitle : @"Donate if you like the app" action : @selector(support) keyEquivalent : @"" ];
+        [ menu addItemWithTitle : @"Check for updates" action : @selector(update) keyEquivalent : @"" ];
+        [ menu addItemWithTitle : @"Quit" action : @selector(terminate) keyEquivalent : @"" ];
 
-        NSImage* image = [ NSImage imageNamed : @"mak" ];
+        NSImage* image = [ NSImage imageNamed : @"icon" ];
         [ image setTemplate : YES ];
 
-        statusItem = [ [ NSStatusBar systemStatusBar ] statusItemWithLength : NSSquareStatusItemLength ];
+        statusItem = [ [ NSStatusBar systemStatusBar ] statusItemWithLength : NSVariableStatusItemLength ];
         [ statusItem setToolTip : @"High Sierra Media Key Enabler" ];
         [ statusItem setMenu : menu ];
         [ statusItem setImage : image ];
         
-        keyTap = [[SPMediaKeyTap alloc] initWithDelegate:self];
-        if([SPMediaKeyTap usesGlobalMediaKeyTap])
-            [keyTap startWatchingMediaKeys];
-        else
-            NSLog(@"Media key monitoring disabled");
+        eventPort = CGEventTapCreate( kCGSessionEventTap, kCGHeadInsertEventTap, kCGEventTapOptionDefault, CGEventMaskBit(NX_SYSDEFINED), tapEventCallback, (__bridge void * _Nullable)(self));
+        eventPortSource = CFMachPortCreateRunLoopSource( kCFAllocatorSystemDefault, eventPort, 0 );
         
+        CFRunLoopAddSource( CFRunLoopGetCurrent(), eventPortSource, kCFRunLoopCommonModes );
+        CFRunLoopRun();
     }
 
-    -(void)mediaKeyTap:(SPMediaKeyTap*)keyTap receivedMediaKeyEvent:(NSEvent*)event;
-    {
-        iTunesApplication* iTunes = [SBApplication applicationWithBundleIdentifier:@"com.apple.iTunes"];
-        SpotifyApplication *spotify = [SBApplication applicationWithBundleIdentifier:@"com.spotify.client"];
-        NSAssert([event type] == NSSystemDefined && [event subtype] == SPSystemDefinedEventMediaKeys, @"Unexpected NSEvent in mediaKeyTap:receivedMediaKeyEvent:");
-        // here be dragons...
-        int keyCode = (([event data1] & 0xFFFF0000) >> 16);
-        int keyFlags = ([event data1] & 0x0000FFFF);
-        BOOL keyIsPressed = (((keyFlags & 0xFF00) >> 8)) == 0xA;
-        
-        if (keyIsPressed) {
-            switch (keyCode) {
-                case NX_KEYTYPE_PLAY:
-                    if ( [iTunes isRunning ] ) [iTunes playpause];
-                    if ( [spotify isRunning ] ) [spotify playpause];
-                    break;
-                    
-                case NX_KEYTYPE_FAST:
-                    if ( [iTunes isRunning ] ) [iTunes nextTrack];
-                    if ( [spotify isRunning ] ) [spotify nextTrack];
-                    break;
-                    
-                case NX_KEYTYPE_REWIND:
-                    if ( [iTunes isRunning ] ) [iTunes previousTrack];
-                    if ( [spotify isRunning ] ) [spotify previousTrack];
-                    break;
-                default:
-                    break;
-                    // More cases defined in hidsystem/ev_keymap.h
-            }
-        }
-    }
-
-- ( void ) terminate
+    - ( void ) terminate
     {
         [ NSApp terminate : nil ];
+    }
+
+    - ( void ) support
+    {
+        [[NSWorkspace sharedWorkspace] openURL:[NSURL URLWithString: @"https://paypal.me/milgra"]];
+    }
+
+    - ( void ) update
+    {
+        [[NSWorkspace sharedWorkspace] openURL:[NSURL URLWithString: @"http://milgra.com/high-sierra-media-key-enabler.html"]];
     }
 
     @end
--- README.md
+++ README.md
@@ -1,8 +1,18 @@
 # highsierramediakeyenabler
-MacOS High Sierra Media Key Enabler for iTunes
 
-It only works with physical buttons, won't work with touchbar.
+What's new in version 1.2 :
+- new icon
+- source code is super tight now
+- developer id signed, its a trusted app now
+
+MacOS High Sierra Media Key Enabler for iTunes and Spotify.
+
+Controls iTunes and Spotify directly. If both is running it controls both.
+
+The app runs in the menu bar in the form of a subtle and beautiful black dot.
+
+Works with physical buttons only, won't work with touchbar MacBooks.
+
+Won't work with headphone remote buttons.
 
 Download the compiled application from my [homepage](http://milgra.com/high-sierra-media-key-enabler.html)
-
-I don't have a developer certificate so trust the app when MacOS asks. Compile the project yourself if you don't trust me :)

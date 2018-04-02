69f9de932162d365de9d512b4e6a67e2260c0b3d
Sungho Lee <sh1217sh@gmail.com>
2018-04-02 15:53:35 +0900

    Manual revert of d697595 "Prevent unexpected behavior of the headphone button"

--- HighSierraMediaKeyEnabler/AppDelegate.m
+++ HighSierraMediaKeyEnabler/AppDelegate.m
@@ -2,7 +2,6 @@
 #import "GBLaunchAtLogin.h"
 #import "iTunes.h"
 #import "Spotify.h"
-#import <MediaPlayer/MediaPlayer.h>
 #import <ScriptingBridge/ScriptingBridge.h>
 
 typedef NS_ENUM(NSInteger, MediaKeysPrioritize)
@@ -289,14 +288,6 @@ static CGEventRef tapEventCallback(CGEventTapProxy proxy, CGEventType type, CGEv
     {
         pauseState = [option integerValue];
     }
-    
-    // Always return success when system media control is sent to the app.
-    // This will prevent unexpected behavior when the headphone button is pressed.
-    
-    [[MPRemoteCommandCenter sharedCommandCenter].togglePlayPauseCommand addTargetWithHandler: ^MPRemoteCommandHandlerStatus(MPRemoteCommandEvent *event)
-     {
-         return MPRemoteCommandHandlerStatusSuccess;
-     }];
     
     // Version string
     
